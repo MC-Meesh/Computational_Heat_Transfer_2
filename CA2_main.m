ddbbe114c61333db2227553396eecdfbd3b303e2
Michael Allen <chaseallenskis@gmail.com>
2023-03-05 10:19:25 -0700

    Init

---
+++ CA2_main.m
@@ -0,0 +1,15 @@
+% Heat Transfer - Spring 2023 Computational Assignment 1
+% Michael Allen, Cullen Hirstius, Hayden Payne
+% 3/5/23
+
+clc; clear;  close all;
+format short 
+
+FIN_COUNT = [1, (3^2), (6^2), (9^2), (12^2)];
+
+for N = FIN_COUNT
+end
+
+
+
+
